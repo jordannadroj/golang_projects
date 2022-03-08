fe49cfa023f7714a2f3d24938b483beb061edce3
jordannadroj <jordansiemens93@gmail.com>
2022-03-08 20:41:34 +0100

    initial commit of 52 projects in 52 weeks
    weather app

---
+++ weather/go.mod
@@ -0,0 +1,5 @@
+module weather
+
+go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ weather/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Response struct {
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Dt  int `json:"dt"`
+	Sys struct {
+		Type    int    `json:"type"`
+		ID      int    `json:"id"`
+		Country string `json:"country"`
+		Sunrise int    `json:"sunrise"`
+		Sunset  int    `json:"sunset"`
+	} `json:"sys"`
+	Timezone int    `json:"timezone"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Cod      int    `json:"cod"`
+}
+
+func getCity(r bufio.Reader) string {
+	fmt.Printf("Type in a city for it's weather > ")
+
+	reader := bufio.NewReader(os.Stdin)
+	city, _ := reader.ReadString('\n')
+	city = city[:len(city)-1]
+	return city
+}
+
+func fetchWeatherData(city string) string {
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%v&units=metric&&appid=%v", city, os.Getenv("WEATHER_APIKEY"))
+
+	//response in is bytes format
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	// we then perform the conversion of our response’s body from bytes into something meaningful that can be printed out in the console.
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+	return fmt.Sprintf("The current temperature in %s is %v°C", city, responseObject.Main.Temp)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	city := getCity(bufio.Reader{})
+
+	weather := fetchWeatherData(city)
+
+	fmt.Println(weather)
+}
