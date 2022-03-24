7cdddaa277e957119e070136acb9bba2e046a963
jordannadroj <jordansiemens93@gmail.com>
2022-03-24 08:55:24 +0100

    testing

--- weather/weather/weather.go
+++ weather/weather/weather.go
@@ -3,54 +3,12 @@ package weather
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 )
-
-type Response struct {
-	Coord struct {
-		Lon float64 `json:"lon"`
-		Lat float64 `json:"lat"`
-	} `json:"coord"`
-	Weather []struct {
-		ID          int    `json:"id"`
-		Main        string `json:"main"`
-		Description string `json:"description"`
-		Icon        string `json:"icon"`
-	} `json:"weather"`
-	Base string `json:"base"`
-	Main struct {
-		Temp      float64 `json:"temp"`
-		FeelsLike float64 `json:"feels_like"`
-		TempMin   float64 `json:"temp_min"`
-		TempMax   float64 `json:"temp_max"`
-		Pressure  int     `json:"pressure"`
-		Humidity  int     `json:"humidity"`
-	} `json:"main"`
-	Visibility int `json:"visibility"`
-	Wind       struct {
-		Speed float64 `json:"speed"`
-		Deg   int     `json:"deg"`
-	} `json:"wind"`
-	Clouds struct {
-		All int `json:"all"`
-	} `json:"clouds"`
-	Dt  int `json:"dt"`
-	Sys struct {
-		Type    int    `json:"type"`
-		ID      int    `json:"id"`
-		Country string `json:"country"`
-		Sunrise int    `json:"sunrise"`
-		Sunset  int    `json:"sunset"`
-	} `json:"sys"`
-	Timezone int    `json:"timezone"`
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Cod      int    `json:"cod"`
-}
 
 func PromptUser() {
 	fmt.Printf("Type in a city for it's weather > ")
@@ -81,11 +39,15 @@ func GetCity() {
 	reader := bufio.NewReader(os.Stdin)
 	city, _ := reader.ReadString('\n')
 	city = city[:len(city)-1]
-	fmt.Println(FetchWeatherData(city))
+	cityWeather, err := FetchWeatherData(city)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(cityWeather)
 	AskAgain()
 }
 
-func FetchWeatherData(city string) string {
+func FetchWeatherData(city string) (string, error) {
 	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%v&units=metric&&appid=%v", city, os.Getenv("WEATHER_APIKEY"))
 
 	//response in is bytes format
@@ -93,17 +55,23 @@ func FetchWeatherData(city string) string {
 	response.Header.Add("Accept", "application/json")
 
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		return "", err
 	}
 
 	// we then perform the conversion of our response’s body from bytes into something meaningful that can be printed out in the console.
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	var responseObject Response
-	json.Unmarshal(responseData, &responseObject)
-	return fmt.Sprintf("The current temperature in %s is %v°C", city, responseObject.Main.Temp)
+	var responseObject WeatherData
+	err = json.Unmarshal(responseData, &responseObject)
+	if err != nil {
+		return "", err
+	}
+	if responseObject.Main.Temp == 0 {
+		fmt.Println(responseObject.Main.Temp)
+		return "", errors.New("invalid city")
+	}
+	return fmt.Sprintf("The current temperature in %s is %v°C", city, responseObject.Main.Temp), nil
 }
--- weather/weather/weather_test.go
+++ weather/weather/weather_test.go
@@ -1,13 +1,43 @@
 package weather
 
 import (
-	"net/http"
-	"net/http/httptest"
+	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestFetchWeatherData(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)
+	//server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)
+	testCases := []struct {
+		name    string
+		city    string
+		jordan  func()
+		wantErr bool
+	}{
+		{
+			"passes",
+			"berlin",
+			func() {
+				fmt.Println("HI")
+			},
+			false,
+		},
+		{
+			"error",
+			"berlins",
+			func() {},
+			true,
+		},
+	}
+	for _, tc := range testCases {
 
+		tc.jordan()
 
+		_, err := FetchWeatherData(tc.city)
+		if tc.wantErr {
+			require.Error(t, err, tc.name)
+		} else {
+			require.Nil(t, err, tc.name)
+		}
+	}
 }
