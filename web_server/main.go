1db55dd38b6539f7d1e30087dfd8b5b06ea2a3a9
jordannadroj <jordansiemens93@gmail.com>
2022-04-11 21:42:16 +0200

    html files

--- web_server/main.go
+++ web_server/main.go
@@ -22,12 +22,17 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil { //r.ParseForm will parse the information passed into the form in /form.html
-		fmt.Fprintf(w, "Parse error")
+		fmt.Fprintf(w, "ParseForm() error %v", err)
 	}
+	fmt.Fprintf(w, "Form submitted\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %v\n", name)
+	fmt.Fprintf(w, "Address = %v", address)
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./web_server")) // .Dir specifies the directory tree of the files to be used
+	fileServer := http.FileServer(http.Dir("../web_server")) // .Dir specifies the directory tree of the files to be used
 
 	http.Handle("/", fileServer)          // create a handle to see root page to server
 	http.HandleFunc("/form", formHandler) // handle /form route
